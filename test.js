f5867fd6f7ab13468f8e98685df828e238490e60
Nikolay Karadzhov <nikolayk@starsgroup.com>
2019-02-01 18:09:42 +0200

    performance calculator

--- test.js
+++ test.js
@@ -18,4 +18,15 @@ const invoices = [
   }
 ]
 
-console.log(statement.statement(invoices[0], plays))
+const test = `Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $450.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,600.00
+You earned 40 credits`
+
+const result = statement.statement(invoices[0], plays)
+
+// console.log(test.length)
+// console.log(result.length)
+assert(result.trim() == test.trim())
